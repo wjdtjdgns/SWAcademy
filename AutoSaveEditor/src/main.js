40bb95a27b5f1cd856a261d14e2d0be5f248e3ae
wjdtjdgns <person3275@gmail.com>
2023-02-03 21:43:01 +0900

    feat: Add PostPage component

--- AutoSaveEditor/src/main.js
+++ AutoSaveEditor/src/main.js
@@ -1,17 +1,5 @@
-import { request } from "./api.js";
-import PostList from "./PostList.js";
+import App from "./App.js";
 
 const $target = document.querySelector("#app");
 
-const postList = new PostList({
-  $target,
-  initialState: [],
-});
-
-const fetchPosts = async () => {
-  const posts = await request("/posts");
-
-  postList.setState(posts);
-};
-
-fetchPosts();
+new App({ $target });
