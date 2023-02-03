538c25f78b97cc96e304c8c7e9d020aa02e2b8dd
wjdtjdgns <person3275@gmail.com>
2023-02-03 21:13:21 +0900

    feat: Add Dummy Data

---
+++ AutoSaveEditor/src/main.js
@@ -0,0 +1,20 @@
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: "test1",
+  },
+  {
+    id: 2,
+    title: "test2",
+  },
+  {
+    id: 3,
+    title: "test3",
+  },
+  {
+    id: 4,
+    title: "test4",
+  },
+];
+
+const $target = document.querySelector("#app");
