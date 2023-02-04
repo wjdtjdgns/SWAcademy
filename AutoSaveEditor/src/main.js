8f92c2bc5a81b83f5f2d0ba99f2de27672cfb0f8
wjdtjdgns <person3275@gmail.com>
2023-02-04 18:25:57 +0900

    feat: Add Editor component

--- AutoSaveEditor/src/main.js
+++ AutoSaveEditor/src/main.js
@@ -1,5 +1,12 @@
 import App from "./App.js";
-
+import PostEditPage from "./PostEditPage.js";
 const $target = document.querySelector("#app");
 
 new App({ $target });
+
+const postEditPage = new PostEditPage({
+  $target,
+  initialState: { postId: "new" },
+});
+
+postEditPage.render();
