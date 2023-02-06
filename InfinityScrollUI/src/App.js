d27e76a7cd25ae23c2ae01c42e6ff3c3e9a49a8c
wjdtjdgns <person3275@gmail.com>
2023-02-06 17:46:50 +0900

    Add: Dummy Data

---
+++ InfinityScrollUI/src/App.js
@@ -0,0 +1,36 @@
+const DUMMY_DATA = [
+  {
+    id: 1,
+    imagePath:
+      "https://s3.ap-northeast-2.amazonaws.com/kdt-cdn.roto.codes/cat-photos/20200428_052455.jpg",
+    cats: "1, 2",
+    created_at: "2021-08-22T12:13:26.364z",
+    updated_at: "2021-08-22T12:26:50.467z",
+    photo_in_cats: [
+      {
+        id: 1,
+        name: "나나",
+        colors: "yellow, white",
+        birthday: null,
+        profileImage:
+          "https://s3.ap-northeast-2.amazonaws.com/kdt-cdn.roto.codes/cat-photos/20201217_012751.jpg",
+        published_at: "2021-08-22T12:09:21.753z",
+        created_at: "2021-08-22T12:09:20.857z",
+        updated_at: "2021-08-22T12:11:16.963z",
+      },
+      {
+        id: 2,
+        name: "모나",
+        colors: "black, white",
+        birthday: null,
+        profileImage:
+          "https://s3.ap-northeast-2.amazonaws.com/kdt-cdn.roto.codes/cat-photos/20210821_075630.jpg",
+        published_at: "2021-08-22T12:09:49.615z",
+        created_at: "2021-08-22T12:09:48.793z",
+        updated_at: "2021-08-22T12:12:14.809z",
+      },
+    ],
+  },
+];
+
+export default function App({ $target }) {}
