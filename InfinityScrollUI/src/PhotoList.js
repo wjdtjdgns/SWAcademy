36d2b86bd3a8442788a94338cbb20d684983f0de
wjdtjdgns <person3275@gmail.com>
2023-02-08 18:20:55 +0900

    feat: 로드 버튼 추가

--- InfinityScrollUI/src/PhotoList.js
+++ InfinityScrollUI/src/PhotoList.js
@@ -1,5 +1,5 @@
 export default function PhotoList({ $target, initialState, onScrollEnded }) {
-  const $photoList = document.createElement("ul");
+  const $photoList = document.createElement("div");
   $target.appendChild($photoList);
   this.state = initialState;
 
@@ -10,18 +10,27 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
 
   this.render = () => {
     $photoList.innerHTML = `
+    <ul>
       ${this.state
         .map(
           (photo) =>
             `
-          <li>
+          <li style="list-style:none;">
             <img width="100%" src="${photo.imagePath}" />
           </li>
         `
         )
         .join("")}
+    </ul>
+    <button class="PhotoList__loadMore" style="width:100%; height 200px; font-size:20px;">Load More</button>
     `;
   };
 
   this.render();
+
+  $photoList.addEventListener("click", (e) => {
+    if (e.target.className === "PhotoList__loadMore") {
+      onScrollEnded();
+    }
+  });
 }
