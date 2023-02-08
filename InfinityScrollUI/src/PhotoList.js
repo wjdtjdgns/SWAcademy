3c9cffbacd3c06ccd2d26e0036f46542fa07fbec
wjdtjdgns <person3275@gmail.com>
2023-02-08 19:40:00 +0900

    fix: PhotoList 다음 리스트 렌더링 방식 scroll -> IntersectionObserver 변경

--- InfinityScrollUI/src/PhotoList.js
+++ InfinityScrollUI/src/PhotoList.js
@@ -4,6 +4,22 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
   $target.appendChild($photoList);
   this.state = initialState;
 
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !this.state.isLoading) {
+          onScrollEnded();
+        }
+      });
+    },
+    {
+      root: null,
+      threshold: 0.5,
+    }
+  );
+
+  let $lastLi = null;
+
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
@@ -26,23 +42,34 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
       if ($photos.querySelector(`li[data-id="${photo.id}"]`) === null) {
         const $li = document.createElement("li");
         $li.setAttribute("data-id", photo.id);
-        $li.style = "list-style: none;";
+        $li.style = "list-style: none; min-height: 800px";
         $li.innerHTML = `<img width="100%" src="${photo.imagePath}" />`;
 
         $photos.appendChild($li);
       }
     });
+
+    const $nextLi = $photos.querySelector("li:last-child");
+
+    if ($nextLi !== null) {
+      if ($lastLi !== null) {
+        observer.unobserve($lastLi);
+      }
+      $lastLi = $nextLi;
+      observer.observe($lastLi);
+    }
   };
 
   this.render();
 
-  window.addEventListener("scroll", () => {
-    const { isLoading, totalCount, photos } = this.state;
-    const isScrollEnded =
-      window.innerHeight + window.scrollY + 100 >= document.body.offsetHeight;
+  // scroll 방식 렌더링
+  // window.addEventListener("scroll", () => {
+  //   const { isLoading, totalCount, photos } = this.state;
+  //   const isScrollEnded =
+  //     window.innerHeight + window.scrollY + 100 >= document.body.offsetHeight;
 
-    if (isScrollEnded && !isLoading && photos.length < totalCount) {
-      onScrollEnded();
-    }
-  });
+  //   if (isScrollEnded && !isLoading && photos.length < totalCount) {
+  //     onScrollEnded();
+  //   }
+  // });
 }
