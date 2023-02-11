04452bf71c3d37fca5793e2b51e52e6e88dae145
wjdtjdgns <person3275@gmail.com>
2023-02-11 15:36:21 +0900

    add: Dummy data 2 추가

--- CatGallery/src/App.js
+++ CatGallery/src/App.js
@@ -45,12 +45,29 @@ const DUMMY_DATA = [
   },
 ];
 
+const DUMMY_DATA_2 = [
+  {
+    id: "5",
+    name: "2021/04",
+    type: "DIRECTORY",
+    filePath: null,
+    parent: { id: "1" },
+  },
+  {
+    id: "19",
+    name: "물 마시는 사진",
+    type: "FILE",
+    filePath: "/images/a2i.jpg",
+    parent: { id: "1" },
+  },
+];
+
 export default function App({ $target }) {
   const nodes = new Nodes({
     $target,
     initialState: {
-      isRoot: true,
-      nodes: DUMMY_DATA,
+      isRoot: false,
+      nodes: DUMMY_DATA_2,
     },
     onClick: () => {},
   });
