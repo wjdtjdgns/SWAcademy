ff76562dd59b95c3363b15f002de049f1e9b7f3a
wjdtjdgns <person3275@gmail.com>
2023-02-11 21:45:35 +0900

    feat: SuggestKeyword 기능 구현

--- CatPhotoSearch/src/App.js
+++ CatPhotoSearch/src/App.js
@@ -1,17 +1,46 @@
 import { request } from "./api.js";
 import Header from "./Header.js";
+import SuggestKeywords from "./SuggestKeywords.js";
 
 export default function App({ $target }) {
   this.state = {
+    keyword: "",
     keywords: [],
   };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    header.setState({
+      keyword: this.state.keyword,
+    });
+    suggestKeywords.setState(this.state.keywords);
+  };
+
   const header = new Header({
     $target,
+    initialState: {
+      keyword: this.state.keyword,
+    },
     onKeywordInput: async (keyword) => {
       if (keyword.trim().length > 1) {
         const keywords = await request(`/keywords?q=${keyword}`);
-        console.log(keywords);
+
+        this.setState({
+          ...this.state,
+          keyword,
+          keywords,
+        });
       }
+    },
+  });
+  const suggestKeywords = new SuggestKeywords({
+    $target,
+    initialState: this.state.keywords,
+    onKeywordSelect: (keyword) => {
+      this.setState({
+        ...this.state,
+        keyword,
+      });
     },
   });
 }
