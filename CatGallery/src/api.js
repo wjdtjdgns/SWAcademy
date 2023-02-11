213aa984a8823694294b338db14450c9501cb0df
wjdtjdgns <person3275@gmail.com>
2023-02-11 15:45:56 +0900

    feat: API 연동

---
+++ CatGallery/src/api.js
@@ -0,0 +1,14 @@
+const API_END_POINT = "https://cat-photos.edu-api.programmers.co.kr";
+export const request = async (url) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`);
+
+    if (!res.ok) {
+      throw new Error("API Call Fail");
+    }
+
+    return await res.json();
+  } catch (e) {
+    alert(e.message);
+  }
+};
