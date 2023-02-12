d6815f27750939cfa64d45d5beeb1a7e8b2f74bf
wjdtjdgns <person3275@gmail.com>
2023-02-12 16:13:28 +0900

    feat: 사진 api 호출

--- CatPhotoSearch/src/App.js
+++ CatPhotoSearch/src/App.js
@@ -1,19 +1,32 @@
 import { request } from "./api.js";
 import Header from "./Header.js";
 import SuggestKeywords from "./SuggestKeywords.js";
+import SearchResults from "./SearchResults.js";
 
 export default function App({ $target }) {
   this.state = {
     keyword: "",
     keywords: [],
+    catImages: [],
   };
 
   this.setState = (nextState) => {
     this.state = nextState;
+
+    // if (this.state.keyword !== nextState.keyword) {
+    //   header.setState({
+    //     keyword: this.state.keyword,
+    //   });
+    // }
     header.setState({
       keyword: this.state.keyword,
     });
+
     suggestKeywords.setState({ keywords: this.state.keywords });
+
+    if (this.state.catImages.length > 0) {
+      searchResults.setState(this.state.catImages);
+    }
   };
 
   const header = new Header({
@@ -32,6 +45,9 @@ export default function App({ $target }) {
         });
       }
     },
+    onEnter: () => {
+      fetchCatsImage();
+    },
   });
   const suggestKeywords = new SuggestKeywords({
     $target,
@@ -43,7 +59,24 @@ export default function App({ $target }) {
       this.setState({
         ...this.state,
         keyword,
+        keywords: [],
       });
+      fetchCatsImage();
     },
   });
+
+  const searchResults = new SearchResults({
+    $target,
+    initialState: this.state.catImages,
+  });
+
+  const fetchCatsImage = async () => {
+    const { data } = await request(`/search?q=${this.state.keyword}`);
+
+    this.setState({
+      ...this.state,
+      catImages: data,
+      keywords: [],
+    });
+  };
 }
