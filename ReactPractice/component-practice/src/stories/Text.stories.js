a95598cf3cad63ddc7041ffe378f95bb5cf2d224
wjdtjdgns <person3275@gmail.com>
2023-02-16 17:15:37 +0900

    add: Text component 추가

---
+++ ReactPractice/component-practice/src/stories/Text.stories.js
@@ -0,0 +1,40 @@
+import Text from "../components/Text";
+
+export default {
+  title: "Component/Text",
+  component: Text,
+  argTypes: {
+    size: { control: "number" },
+    strong: { control: "boolean" },
+    underline: { control: "boolean" },
+    delete: { control: "boolean" },
+    color: { control: "color" },
+    block: { control: "boolean" },
+    paragraph: { control: "boolean" },
+    mark: { control: "boolean" },
+    code: { control: "boolean" },
+  },
+};
+
+export const Default = (args) => {
+  return <Text {...args}>Text</Text>;
+};
+
+export const Size = (args) => {
+  return (
+    <>
+      <Text {...args} size="large">
+        Text
+      </Text>
+      <Text {...args} size="normal">
+        Text
+      </Text>
+      <Text {...args} size="small">
+        Text
+      </Text>
+      <Text {...args} size={24}>
+        Text
+      </Text>
+    </>
+  );
+};
